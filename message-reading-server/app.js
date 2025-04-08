45b9f0c3d85e08c1beac63123b1579e3a58c364b
eastshine-high <eastshine.high@gmail.com>
2025-04-09 00:01:58 +0900

    route 연결

--- message-reading-server/app.js
+++ message-reading-server/app.js
@@ -1,13 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,7 +17,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var indexRouter = require('./routes/index');
 app.use('/', indexRouter);
+
+var usersRouter = require('./routes/users');
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
